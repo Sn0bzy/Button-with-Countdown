f835a01614d58cdb84561635b0cf3c0b01e35594
Hasan CAN <hasancan.net.tr@gmail.com>
2017-08-27 16:29:42 +0300

    Created

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="tr">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Sayaçlı İndirme Butonu</title>
+		<link rel="stylesheet" href="https://cdn.rawgit.com/twbs/bootstrap/v4-dev/dist/css/bootstrap.css">
+	</head>
+	<body>
+	<div class="container">
+		<br/><br/>
+		<h1 class="text-xs-center">Sayaçlı İndirme Butonu</h1>
+		<p class="text-xs-center">Süre sona erdiğinde indirme butonu aktif olacaktır. Uygulamayı <a href="https://github.com/okandiyebiri/sayacli-indirme-butonu" target="blank">GitHub</a>'dan indirebilir veya geliştirebilirsiniz.</p>
+		<hr>
+		<center>
+		<form name="okn">
+		<input name="submitbtn" class="btn btn-primary text-xs-center" onclick="location.href='https://okandiyebiri.com'" type="button" value="İNDİR" />
+		</form>
+		</center>
+	</div>
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+	<script type="text/javascript" src="sayac.js"></script>
+	</body>
+</html>
