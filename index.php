40fc2cc46b448c47c275ebc5e48e0d4533021ff1
Hasan CAN <hasancan.net.tr@gmail.com>
2018-06-08 13:02:22 +0300

    Update index.php

--- index.php
+++ index.php
@@ -4,23 +4,23 @@
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<title>Sayaçlı İndirme Butonu</title>
+		<title>Geri Sayımlı Buton</title>
 		<link rel="stylesheet" href="https://cdn.rawgit.com/twbs/bootstrap/v4-dev/dist/css/bootstrap.css">
 	</head>
 	<body>
 	<div class="container">
 		<br/><br/>
-		<h1 class="text-xs-center">Sayaçlı İndirme Butonu</h1>
-		<p class="text-xs-center">Süre sona erdiğinde indirme butonu aktif olacaktır. Uygulamayı <a href="https://github.com/okandiyebiri/sayacli-indirme-butonu" target="blank">GitHub</a>'dan indirebilir veya geliştirebilirsiniz.</p>
+		<h1 class="text-xs-center">Geri Sayımlı Buton</h1>
+		<p class="text-xs-center">Geri sayım bittiğinde buton aktif ve tıklanabilir olacaktır..</p>
 		<hr>
 		<center>
-		<form name="okn">
-		<input name="submitbtn" class="btn btn-primary text-xs-center" onclick="location.href='https://okandiyebiri.com'" type="button" value="İNDİR" />
+		<form name="countdown-hc">
+		<input name="submitbtn" class="btn btn-primary text-xs-center" onclick="location.href='https://hasan.im'" type="button" value="Git" />
 		</form>
 		</center>
 	</div>
 
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-	<script type="text/javascript" src="sayac.js"></script>
+	<script type="text/javascript" src="countdown.js"></script>
 	</body>
 </html>
